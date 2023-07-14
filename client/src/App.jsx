146ba68a7b8fe0f64d01f1e469c2ae80021233f3
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-07-14 23:44:45 +0530

    feat : footer added ✔

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Routes, Route } from "react-router-dom"
-import Home from './Pages/Home'
-import Navbar from './Components/Navbar'
-import Login from './Components/Auth/Login'
-import Signup from './Components/Auth/Signup'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import Login from "./Components/Auth/Login";
+import Signup from "./Components/Auth/Signup";
 import ProtectedRoutes from "./Pages/ProtectedRoutes";
-import PrivateRoutes from './Pages/PrivateRoutes'
-import ErrorBoundary from './Pages/ErrorBoundary'
-import Homepage from './Pages/HomePage'
-import StudentHome from './Pages/StudentHome'
-import StudentForm from './Components/Student/StudentForm'
-import EditStudent from './Components/Student/EditStudent'
+import PrivateRoutes from "./Pages/PrivateRoutes";
+import ErrorBoundary from "./Pages/ErrorBoundary";
+import Homepage from "./Pages/HomePage";
+import StudentHome from "./Pages/StudentHome";
+import StudentForm from "./Components/Student/StudentForm";
+import EditStudent from "./Components/Student/EditStudent";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   // const token = cookies.get("TOKEN");
   return (
-<>
+    <>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route element={<PrivateRoutes />}>
+          <Route
+            path="Home"
+            element={
+              <>
+                <Navbar />
+                <Home />
+              </>
+            }
+          ></Route>
+          <Route path="Student" element={<StudentHome />}></Route>
+        </Route>
 
-
-<Routes>
-
-<Route path="/" element={<Homepage/>}></Route>
-  <Route element={<PrivateRoutes/>}>
-    <Route path='Home'element={<><Navbar/><Home/></> }></Route>
-    <Route path='Student'element={<StudentHome/>}></Route>
-
-  </Route>
-
-  <Route path="/Login" element={<><Navbar/><Login/></>}></Route>
-  <Route path="/Signup" element={<><Navbar/> <Signup/></>}></Route>
-  <Route path="/add-student" element={<StudentForm/>}></Route>
-  <Route path="/edit/:id" element={<EditStudent/>}></Route>
-</Routes>
-</>
-  )
+        <Route
+          path="/Login"
+          element={
+            <>
+              <Navbar />
+              <Login />
+            </>
+          }
+        ></Route>
+        <Route
+          path="/Signup"
+          element={
+            <>
+              <Navbar /> <Signup />
+            </>
+          }
+        ></Route>
+        <Route path="/add-student" element={<StudentForm />}></Route>
+        <Route path="/edit/:id" element={<EditStudent />}></Route>
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -4,7 +4,7 @@ import Navbar from "../Components/Navbar";
 export default function Homepage() {
   return (
     <div>
-        <Navbar></Navbar>
+      <Navbar></Navbar>
       <section>
         <div class="bg-white dark:bg-gray-900">
           <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
